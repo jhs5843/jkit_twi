fe6899d40a38cd9e9d968f5220342e6ab0616c35
HeungsikJo <jhs5843@naver.com>
2018-11-07 09:31:40 +0900

    first commit

---
+++ jkit_twi/jkit_twi/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * jkit_twi.cpp
+ *
+ * Created: 2018-11-07 오전 9:29:01
+ * Author : USER
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
